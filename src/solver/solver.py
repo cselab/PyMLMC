a17ae8cd2e912041a82421cbf94a08fc47bcd2d6
Jonas Šukys <sukys.jonas@gmail.com>
2014-10-06 12:27:23 +0200

    std

--- src/solver/solver.py
+++ src/solver/solver.py
@@ -13,11 +13,3 @@ class Solver (object):
   # return the name of a particular run
   def name (self, level, type, sample, id):
     return 'level=%d_type=%d_sample=%d_id=%d' % (level, type, sample, id)
-  
-  # return the seed for the simulation
-  def seed (self, level, sample, id):
-    return self.pair ( self.pair (level, sample), id )
-  
-  # pair two seeds into one
-  def pair (self, a, b):
-    return a ** 2 + a + b if a >= b else a + b ** 2
