3ed1aa8b65691ee9d50e7711961e8935813407bc
Jonas Šukys <sukys.jonas@gmail.com>
2014-12-02 15:09:57 +0100

    std

--- src/scheduler/scheduler_static.py
+++ src/scheduler/scheduler_static.py
@@ -32,7 +32,8 @@ class Static (Scheduler):
       cores = max ( min ( local.threads, self.cores ), int ( round ( required ) ) )
       
       # walltime is decreased due to level (w.r.t to L) and increased due to fewer cores
-      walltime = self.walltime * (float(self.works [level - type]) / self.works [self.L]) * (float(self.cores) / cores) 
+      walltime = self.walltime * (float(self.works [level - type]) / self.works [self.L]) * (float(self.cores) / required)
+      #walltime = self.walltime * (float(self.works [level - type]) / self.works [self.L]) * (float(self.cores) / cores)
       
       # process in batch all levels, except the 'self.separate' finest ones
       batch = ( level - type <= self.L - self.separate )
