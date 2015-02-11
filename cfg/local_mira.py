eebf0dd3a65f22edc00e7c0adb7c68519928e061
Jonas Šukys <sukys.jonas@gmail.com>
2015-02-11 11:28:52 +0100

    std

--- cfg/local_mira.py
+++ cfg/local_mira.py
@@ -34,8 +34,8 @@ min_cores    = 512 * cores
 peakflops = 0.0 # TFLOP/s
 bandwidth = 0.0 # GB/s
 
-# scratch path (not required - we use $WORK)
-scratch = None
+# scratch path
+scratch = '/projects/CloudPredict/sukysj/pymlmc'
 
 # default environment variables
 envs = '''  --envs PAMI_DEVICE=B \
