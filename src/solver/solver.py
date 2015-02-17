89867026ffb4c14e5740badf614c18ca47273d34
Jonas Šukys <sukys.jonas@gmail.com>
2015-02-17 08:38:39 +0100

    std

--- src/solver/solver.py
+++ src/solver/solver.py
@@ -388,6 +388,7 @@ class Solver (object):
     
     # read timer file
     timerfilepath = os.path.join (directory, self.timerfile % label)
+    print timerfilepath
     if os.path.exists (timerfilepath):
       with open ( os.path.join (directory, self.timerfile % label), 'r' ) as f:
         line = f.readlines() [-3]
