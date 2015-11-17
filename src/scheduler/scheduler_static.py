0af45740ce8fe5a3a168bb75293a7272c8d738c0
Jonas Šukys <sukys.jonas@gmail.com>
2015-11-17 14:46:16 +0100

    std

--- src/scheduler/scheduler_static.py
+++ src/scheduler/scheduler_static.py
@@ -32,7 +32,7 @@ class Static (Scheduler):
 
     for level, type in self.levels_types:
       
-      required = self.cores / ( float (self.ratios [self.L]) / float (self.ratios [level - type]) )
+      required = self.cores / float (self.ratios [level - type])
 
       # respect the minimal amount of cores on the machine
       cores = max ( min ( local.min_cores, self.cores ), int ( round ( required ) ) )
