7d9d88e8651185d85339de957ed211a58128bd63
Jonas Šukys <sukys.jonas@gmail.com>
2015-11-20 00:02:35 +0100

    std

--- src/helpers.py
+++ src/helpers.py
@@ -32,7 +32,7 @@ def parse ():
   parser.add_argument ('-s', '--simulate',      action = "count", default = 0,  help = 'simulate run only - no actual execution')
   parser.add_argument ('-p', '--proceed',       action = "count", default = 0,  help = 'proceed with simulations (might override existing files)')
   parser.add_argument ('-o', '--override',      action = "count", default = 0,  help = 'override configurations')
-  parser.add_argument ('-n', '--no-init',       action = "count", default = 0,  help = 'do not execute solver init scripts')
+  parser.add_argument ('-n', '--noinit',        action = "count", default = 0,  help = 'do not execute solver init scripts')
   parser.add_argument ('-b', '--batch',         action = "store", default = 1,  help = 'group small jobs of the same level and type into a single batch job', type=int)
 
   global params
