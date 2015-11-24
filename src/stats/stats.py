6ceb223402188e0f7938118d0ff24001f8d4a87c
Jonas Šukys <sukys.jonas@gmail.com>
2015-11-24 11:12:25 +0100

    std

--- src/stats/stats.py
+++ src/stats/stats.py
@@ -15,8 +15,10 @@ class Stat (object):
   # TODO: implement serialize() method for DataClass and generalize this
   def compute_all (self, samples):
     
-    # copy data class from the first sample
-    stats = copy.deepcopy (samples [0])
+    # copy data class from the first valid sample
+    for sample in samples:
+      if sample != None:
+        stats = copy.deepcopy (sample)
     
     # compute sample statistics
     for key in stats.data.keys():
