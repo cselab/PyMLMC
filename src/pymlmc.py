88980eb2a795dace5afc544fa6da22b551b460b4
Jonas Šukys <sukys.jonas@gmail.com>
2016-01-06 17:35:32 +0100

    std

--- src/pymlmc.py
+++ src/pymlmc.py
@@ -328,10 +328,10 @@ class MLMC (object):
     header    = '  :  LEVEL  |   TYPE   |  RESOLUTION  |  SAMPLES  |  HARDWARE  |'
     separator = '  :------------------------------------------------------------|'
     if local.cluster:
-      header    += '  WALLTIME  |  BATCH  |  JOBS   |'
+      header    += '  WALLTIME  |  BATCH  &  JOBS   |'
       separator += '--------------------------------|'
       if local.ensembles:
-        header    += '  MERGE  |  ENSEMBLES  '
+        header    += '  MERGE  &  ENSEMBLES  '
         separator += '-----------------------'
     print header
     print separator
