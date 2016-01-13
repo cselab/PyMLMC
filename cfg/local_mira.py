e6f737b06df395e371a4eed72a1aa75e740843b2
Jonas Šukys <sukys.jonas@gmail.com>
2016-01-13 11:47:23 +0100

    std

--- cfg/local_mira.py
+++ cfg/local_mira.py
@@ -61,54 +61,55 @@ ensembles = 1
 
 # default environment variables
 envs = '''  --envs PAMI_DEVICE=B \
-  --envs BG_MEMSIZE=16384 \
-  --envs BG_THREADLAYOUT=2 \
-  --envs OMP_STACKSIZE=4M \
-  --envs OMP_SCHEDULE="dynamic,1" \
-  --envs PAMID_COLLECTIVES=1 \
-  --envs PAMI_MEMORY_OPTIMIZED=1 \
-  --envs BG_SHAREDMEMSIZE=512 \
-  --envs BG_MAPCOMMONHEAP=0 \
-  --envs BG_SMP_FAST_WAKEUP=YES \
-  --envs L1P_POLICY="dcbt" \
-  --envs L1P_DEPTH=2 \
-  --envs PAMID_THREAD_MULTIPLE=1 \
-  --envs PAMID_VERBOSE=1 \
-  --envs PAMID_MAX_COMMTHREADS=1 \
-  --envs OMP_WAIT_POLICY=PASSIVE \
-  --envs OMP_PROC_BIND=FALSE \
-  --envs USEMAXTHREADS=0 \
-  --envs MYROUNDS=1 \
-  --envs DARSHAN_DISABLE=1 \
-  '''
+--envs BG_MEMSIZE=16384 \
+--envs BG_THREADLAYOUT=2 \
+--envs OMP_STACKSIZE=4M \
+--envs OMP_SCHEDULE="dynamic,1" \
+--envs PAMID_COLLECTIVES=1 \
+--envs PAMI_MEMORY_OPTIMIZED=1 \
+--envs BG_SHAREDMEMSIZE=512 \
+--envs BG_MAPCOMMONHEAP=0 \
+--envs BG_SMP_FAST_WAKEUP=YES \
+--envs L1P_POLICY="dcbt" \
+--envs L1P_DEPTH=2 \
+--envs PAMID_THREAD_MULTIPLE=1 \
+--envs PAMID_VERBOSE=1 \
+--envs PAMID_MAX_COMMTHREADS=1 \
+--envs OMP_WAIT_POLICY=PASSIVE \
+--envs OMP_PROC_BIND=FALSE \
+--envs USEMAXTHREADS=0 \
+--envs MYROUNDS=1 \
+--envs DARSHAN_DISABLE=1 \
+'''
 
 #  --block $COBALT_PARTNAME ${COBALT_CORNER:+--corner} $COBALT_CORNER ${COBALT_SHAPE:+--shape} $COBALT_SHAPE \
 #  --envs PAMID_ASYNC_PROGRESS=1 \
 #  --mapping TABCDE \
 
 # simple run command
-simple_job = '''ulimit -c 0; runjob \
-  --np %(ranks)d \
-  --ranks-per-node %(tasks)d \
-  --cwd $PWD \
-  --envs OMP_NUM_THREADS=%(threads)d \
-  --envs XLSMPOPTS=parthds=%(threads)d \
-  --block BATCH_JOB_BLOCK_HOOK \
-  %(envs)s \
-  : %(cmd)s %(options)s
-  '''
+simple_job = '''ulimit -c 0
+runjob \
+--np %(ranks)d \
+--ranks-per-node %(tasks)d \
+--cwd $PWD \
+--envs OMP_NUM_THREADS=%(threads)d \
+--envs XLSMPOPTS=parthds=%(threads)d \
+--block BATCH_JOB_BLOCK_HOOK \
+%(envs)s \
+: %(cmd)s %(options)s
+'''
 
 # MPI run command
 mpi_job = '''ulimit -c 0; runjob \
-  --np %(ranks)d \
-  --ranks-per-node %(tasks)d \
-  --cwd $PWD \
-  --envs OMP_NUM_THREADS=%(threads)d \
-  --envs XLSMPOPTS=parthds=%(threads)d \
-  --block BATCH_JOB_BLOCK_HOOK \
-  %(envs)s \
-  : %(cmd)s %(options)s
-  '''
+--np %(ranks)d \
+--ranks-per-node %(tasks)d \
+--cwd $PWD \
+--envs OMP_NUM_THREADS=%(threads)d \
+--envs XLSMPOPTS=parthds=%(threads)d \
+--block BATCH_JOB_BLOCK_HOOK \
+%(envs)s \
+: %(cmd)s %(options)s
+'''
 
 # batch job block hook
 BATCH_JOB_BLOCK_HOOK = '${BLOCKS[%(batch_id)d]}'
