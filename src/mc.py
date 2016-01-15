ba2b61fdc0d8d35d789b9472682e08d161fa3b8b
Jonas Šukys <sukys.jonas@gmail.com>
2016-01-15 14:16:49 +0100

    std

--- src/mc.py
+++ src/mc.py
@@ -118,7 +118,7 @@ class MC (object):
       progress.update (step + 1)
 
     # reset progress indicator
-    progress.message ('Finalizing...')
+    progress.message ('finalizing...')
 
     # finalize solver
     info_solver = config.solver.finalize (config.level, config.type, self.parallelization)
