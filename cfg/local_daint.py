accd559890ace274b95fc7896c53d6be1b90cd97
Jonas Šukys <sukys.jonas@gmail.com>
2016-06-25 22:28:02 +0200

    std

--- cfg/local_daint.py
+++ cfg/local_daint.py
@@ -72,7 +72,7 @@ script = '''#!/bin/bash
 #SBATCH --time=%(hours).2d:%(minutes).2d:00
 #SBATCH --mem=%(memory)d
 #SBATCH --output=%(reportfile)s
-#SBATCH --account=s500
+#SBATCH --account=s659
 %(xopts)s
 ulimit -c 0
 %(job)s
