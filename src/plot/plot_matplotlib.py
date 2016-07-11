32cc342e24a5a04e10a9d5d3b5e9e76221a23e84
Jonas Šukys <sukys.jonas@gmail.com>
2016-07-11 11:25:16 +0200

    std

--- src/plot/plot_matplotlib.py
+++ src/plot/plot_matplotlib.py
@@ -1466,7 +1466,7 @@ class MatPlotLib (object):
     pylab.title  ('Rel. level means magnitude for Q = %s' % name (qoi))
     pylab.ylabel (r'|mean of relative $\alpha_\ell Q_\ell - \alpha_{\ell-1} Q_{\ell-1}$|')
     pylab.xlabel ('mesh level')
-    ymin = numpy.max ( [ numpy.min (mean_diff_measured), numpy.min (mean_diff_infered), numpy.min (mean_diff_opt_infered) ] ) / NORMALIZATION
+    ymin = numpy.min ( [ numpy.min (mean_diff_measured), numpy.min (mean_diff_infered), numpy.min (mean_diff_opt_infered) ] ) / NORMALIZATION
     ymax = numpy.max ( [ numpy.max (mean_diff_measured), numpy.max (mean_diff_infered), numpy.max (mean_diff_opt_infered) ] ) / NORMALIZATION
     adjust_extent ([ymin, ymax], factor=1.5)
     levels_extent (levels)
@@ -1483,7 +1483,7 @@ class MatPlotLib (object):
     pylab.title  ('Rel. level std. devs. for Q = %s' % name (qoi))
     pylab.ylabel (r'std. dev. of rel. $\alpha_\ell Q_\ell - \alpha_{\ell-1} Q_{\ell-1}$')
     pylab.xlabel ('mesh level')
-    ymin = numpy.sqrt ( numpy.max ( [ numpy.min (variance_diff_measured), numpy.min (variance_diff_infered), numpy.min (variance_diff_opt_infered) ] ) ) / NORMALIZATION
+    ymin = numpy.sqrt ( numpy.min ( [ numpy.min (variance_diff_measured), numpy.min (variance_diff_infered), numpy.min (variance_diff_opt_infered) ] ) ) / NORMALIZATION
     ymax = numpy.sqrt ( numpy.max ( [ numpy.max (variance_diff_measured), numpy.max (variance_diff_infered), numpy.max (variance_diff_opt_infered) ] ) ) / NORMALIZATION
     adjust_extent ([ymin, ymax], factor=1.5)
     levels_extent (levels)
