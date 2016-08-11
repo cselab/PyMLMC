e79fbf6b83b4cbef65baa871d512cfce947cc416
Jonas Šukys <sukys.jonas@gmail.com>
2016-08-11 20:12:05 +0200

    std

--- src/pymlmc.py
+++ src/pymlmc.py
@@ -801,7 +801,7 @@ class MLMC (object):
       # report missing levels
       else:
 
-        helpers.warning ('Level %d is missing in MLMC assembly - additional bias is present!' % level)
+        helpers.warning ('Level %d is missing in MLMC assembly, leading to additional bias!' % level)
     
     # assemble MLMC estimates (sum of differences for each statistic)
     print '  : MLMC estimates...'
