d5ad0b1481898a4c36e32f0f973da27a698c21b6
Jonas Šukys <sukys.jonas@gmail.com>
2016-11-03 23:18:34 +0100

    std

--- src/coefficients.py
+++ src/coefficients.py
@@ -61,6 +61,9 @@ class Coefficients (object):
   # optimization can be performed for required specific sample-scaled indicators if sample numbers on each level are provided
   def optimize (self, indicators, samples=[]):
 
+    # debug
+    print 'optimizing!'
+
     # no optimization if only one level is present
     if len (self.levels) == 1:
       return
